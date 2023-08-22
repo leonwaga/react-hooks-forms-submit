2841c371c258d2fc77781a99324d1513177fdc25
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-08-22 23:43:45 +0300

    completed assignmnet

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
-
+  const[submittedData, setSubmittedData] = useState([]);
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
   }
@@ -11,6 +11,31 @@ function Form(props) {
   function handleLastNameChange(event) {
     setLastName(event.target.value);
   }
+
+  const [errors, setErrors] = useState([]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (firstName.length > 0) {
+      const formData = {firstName: firstName, lastName: lastName};
+      const dataArray = [...submittedData, formdata];
+      setSubmittedData("");
+      setFirstName("");
+      setLastName("");
+      setErrors([]);
+    } else {
+      setErrors(["First name is required!"]);
+    }
+  }
+
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
 
   return (
     <form>
